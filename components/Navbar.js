685850e25f8f4c32208411bd5e25052bb20a408c
Nicholas Zhang <zhang.nicholas136@gmail.com>
2021-08-06 15:50:24 -0700

    Adding Engines

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-4">
-      <div class="container-fluid">
-        <a class="navbar-brand text-4xl" href="/">
-          <img
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-4">
+      <div className="container-fluid">
+        <a className="navbar-brand text-4xl" href="/">
+          <Image
             src="/weather.png"
             alt=""
             width="50"
             height="40"
-            class="d-inline-block align-text-top mr-3"
+            className="d-inline-block align-text-top mr-3"
           />
           World Weather
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarColor01"
@@ -23,7 +23,7 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
       </div>
     </nav>
